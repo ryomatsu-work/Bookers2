5215248f43f9cf37b00566683fa27b485f3607a2
Ryo Matsufuji <139543423+ryomatsu-work@users.noreply.github.com>
2023-07-21 09:50:14 +0000

    add comment function

---
+++ app/controllers/book_comments_controller.rb
@@ -0,0 +1,29 @@
+class BookCommentsController < ApplicationController
+  before_action :ensure_correct_user, only: [:destroy]
+
+  def create
+    book = Book.find(params[:book_id])
+    comment = current_user.book_comments.new(book_comment_params)
+    comment.book_id = book.id
+    comment.save
+    redirect_to request.referer
+  end
+
+  def destroy
+    BookComment.find(params[:id]).destroy
+    redirect_to request.referer
+  end
+
+  private
+
+  def book_comment_params
+    params.require(:book_comment).permit(:comment)
+  end
+
+  def ensure_correct_user
+    @book_comment = BookComment.find(params[:id])
+    unless @book_comment.user_id == current_user.id
+      redirect_to request.referer
+    end
+  end
+end
