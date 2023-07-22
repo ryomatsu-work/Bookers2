332fb827c950e9c6ebb5588afe03c554395dd737
Ryo Matsufuji <139543423+ryomatsu-work@users.noreply.github.com>
2023-07-22 09:50:19 +0000

    add search

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
 
+  get "search" => 'searches#search', as: :search
+
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
