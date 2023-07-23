c32220552df8cd1b6baf1321de9586127954cea9
Ryo Matsufuji <139543423+ryomatsu-work@users.noreply.github.com>
2023-07-23 09:09:33 +0000

    ajax comments

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,16 +2,18 @@ class BookCommentsController < ApplicationController
   before_action :ensure_correct_user, only: [:destroy]
 
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to request.referer
+    @book = Book.find(params[:book_id])
+    @book_comment = current_user.book_comments.new(book_comment_params)
+    @book_comment.book_id = @book.id
+    @book_comment.save
   end
 
   def destroy
     BookComment.find(params[:id]).destroy
-    redirect_to request.referer
+    @book = Book.find(params[:book_id])
+    @book_comment = BookComment.new
+    render :create
+    # redirect_to request.referer
   end
 
   private
